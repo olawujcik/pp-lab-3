053e3f32388fde8a443db7453296d9b39ee6056b
Aleksandra Wojcik <olawujcik@wp.pl>
2024-04-07 15:58:13 +0200

    zad3

--- geometry/Main.java
+++ geometry/Main.java
@@ -3,11 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Point center = new Point (3.0, 4.0);
 
-        Circle circle = new Circle(center, 5.0); 
+        Circle circle = new Circle(center, 5.0);
+        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0,"yellow"); 
 
         System.out.println("Współrzędne środka koła: (" + circle.getCenter().getX() +", " + circle.getCenter().getY() + ") \n");
         System.out.println("Promień koła: " + circle.getRadius() + "\n");
         System.out.println("Obwód koła: " + circle.calculatePerimeter() + "\n");
-        System.out.println("Pole powierzchni koła: " + circle.getArea() + "\n"); 
+        System.out.println("Pole powierzchni koła: " + circle.getArea() + "\n");
+        System.out.println("Kolor koła: " + coloredCircle.getColor()); 
     }
 }
