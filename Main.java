0757ac966d4768112bde0fa4bf14a0a1cff7a7e4
Aleksandra Wojcik <olawujcik@wp.pl>
2024-04-07 14:50:01 +0200

    chore: add new file

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(5.0,7.0);
+        System.out.println("Współrzędne punktu: " + point.getX() + ", " + point.getY() + "\n");    
+    }
+}
